6f23eff838b07a8daf66b84ba7e3be50ea5f8d80
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2021-04-23 22:53:44 +0300

    Update İki Matris Toplamını .c

--- Lineer-Algebra-master/İki Matris Toplamını .c
+++ Lineer-Algebra-master/İki Matris Toplamını .c
@@ -19,13 +19,13 @@ int main () {
       }
     }
 
-      for(i=0;i<N;i++){
-        for(j=0;j<M;j++){
-          c[i][j] = a[i][j] + b[i][j];
-	  printf("%3d",c[i][j]);
-      	}
-      	printf("\n");
-      }
+     for(i=0;i<N;i++){
+       for(j=0;j<M;j++){
+         c[i][j] = a[i][j] + b[i][j];
+	 printf("%3d",c[i][j]);
+       }
+       printf("\n");
+     }
 
 
   return 0;
